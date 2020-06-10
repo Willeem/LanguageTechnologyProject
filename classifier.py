c1923b14f2a42944472f83aae25f2d46a97ad857
Willeem <willem.datema@hotmail.com>
2020-06-10 16:53:41 +0200

    ready for peregrine?

--- classifier.py
+++ classifier.py
@@ -7,16 +7,17 @@ import pandas as pd
 import argparse
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 from simpletransformers.classification import ClassificationModel
 
 
 def read_data():
     train_en = [[line.rstrip(), 0] for line in open('data/train_src_en.txt', encoding='utf-8').readlines()]
     train_deepl = [[line.rstrip(), 1] for line in open('data/train_deepL_en.txt', encoding='utf-8').readlines()]
-    dev_en = [[line.rstrip(), 1] for line in open('data/dev_src_en.txt', encoding='utf-8').readlines()]
+    dev_en = [[line.rstrip(), 0] for line in open('data/dev_src_en.txt', encoding='utf-8').readlines()]
     dev_deepl = [[line.rstrip(), 1] for line in open('data/dev_deepL_en.txt', encoding='utf-8').readlines()]
     test_en = [[line.rstrip(), 0] for line in open('data/test_src_en.txt', encoding='utf-8').readlines()]
-    test_deepl = [[line.rstrip(), 1] for line in open('data/test_deepl_en.txt', encoding='utf-8').readlines()]
+    test_deepl = [[line.rstrip(), 1] for line in open('data/test_deepL_en.txt', encoding='utf-8').readlines()]
 
     return train_en + train_deepl, dev_en + dev_deepl, test_en + test_deepl
 
@@ -27,11 +28,6 @@ if __name__ == '__main__':
 
     train, dev, test = read_data()
 
-    # shuffle data
-    np.random.shuffle(train)
-    np.random.shuffle(dev)
-    np.random.shuffle(test)
-
     # Turn into pandas dataframes so SimpleTransformers can use the data
     train = pd.DataFrame(train)
     dev = pd.DataFrame(dev)
@@ -39,12 +35,14 @@ if __name__ == '__main__':
 
     # TODO: Hier kunnen we wat data-info toevoegen (label distribution e.d.)
 
-    model = ClassificationModel('roberta', 'roberta-base')
-
+    model_BERT = ClassificationModel('bert', 'bert-base-cased' , use_cuda=True,  args={'num_train_epochs':10, 'train_batch_size': 32, 'eval_batch_size':32, 'output_dir': 'bert_outputs_10_epoch/', 'cache_dir': 'cache_bert_10/'})
     # Train the model
-    model.train_model(train)
+    model_BERT.train_model(train)
 
     # Evaluate the model
-    result, model_outputs, wrong_predictions = model.eval_model(dev)
+    result, model_BERT_outputs, wrong_predictions_BERT = model_BERT.eval_model(dev, cr=classification_report, cm=confusion_matrix, acc=accuracy_score, verbose=True)
+    print(result['cr'])  # Classification Report
+    print(result['cm'])  # Confusion Matrix
+    print(result['acc'])  # Accuracy Score  
 
 
