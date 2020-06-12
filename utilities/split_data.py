d129f41e9a1e0be724b314be91a0be663beed032
Stijn Eikelboom <me@stijneikelboom.nl>
2020-06-12 15:13:33 +0200

    Cleaned up repository

--- utilities/split_data.py
+++ utilities/split_data.py
@@ -3,11 +3,11 @@ from sklearn.model_selection import train_test_split
 
 def main():
     # Load train-dev
-    with open('data/traindev_deepL_en.txt') as traindev_deepL:
+    with open('../data/traindev_deepL_en.txt') as traindev_deepL:
         Xtrain_dev_deepL = traindev_deepL.readlines()
-    with open('data/traindev_src_en.txt') as traindev_src:
+    with open('../data/traindev_src_en.txt') as traindev_src:
         Xtrain_dev_src = traindev_src.readlines()
-    with open('data/traindev_src_de.txt') as traindev_de:
+    with open('../data/traindev_src_de.txt') as traindev_de:
         Xtrain_dev_de = traindev_de.readlines()
 
     # Perform split
@@ -15,19 +15,19 @@ def main():
         Xtrain_dev_deepL, Xtrain_dev_src, Xtrain_dev_de, test_size=0.20, random_state=42)
 
     # Save train
-    with open('data/train_deepL_en.txt', 'w') as train_deepL:
+    with open('../data/train_deepL_en.txt', 'w') as train_deepL:
         train_deepL.write(''.join(Xtrain_deepL))
-    with open('data/train_src_en.txt', 'w') as train_src:
+    with open('../data/train_src_en.txt', 'w') as train_src:
         train_src.write(''.join(Xtrain_src))
-    with open('data/train_src_de.txt', 'w') as train_de:
+    with open('../data/train_src_de.txt', 'w') as train_de:
         train_de.write(''.join(Xtrain_de))
 
     # Save dev
-    with open('data/dev_deepL_en.txt', 'w') as dev_deepL:
+    with open('../data/dev_deepL_en.txt', 'w') as dev_deepL:
         dev_deepL.write(''.join(Xdev_deepL))
-    with open('data/dev_src_en.txt', 'w') as dev_src:
+    with open('../data/dev_src_en.txt', 'w') as dev_src:
         dev_src.write(''.join(Xdev_src))
-    with open('data/dev_src_de.txt', 'w') as dev_de:
+    with open('../data/dev_src_de.txt', 'w') as dev_de:
         dev_de.write(''.join(Xdev_de))
 
 
